3714019f9eb960cd643b16ad86e2a18653242c20
estevesdev <sauloestevesdealmeida27@gmail.com>
2026-02-12 14:44:46 -0400

    feat: implementa rotas de update e delete finalizando o CRUD

--- src/routes.ts
+++ src/routes.ts
@@ -43,4 +43,56 @@ export async function routes(app: FastifyTypeInstance) {
 
     return reply.status(201).send({});
   });
+
+  
+// Rota de DELETAR (DELETE)
+  app.delete('/users/:id', {
+    schema: {
+      description: 'Deletar um usuário pelo ID',
+      tags: ['users'],
+      params: z.object({
+        id: z.string().uuid(), // Valida se é um UUID válido
+      }),
+      response: {
+        204: z.null().describe('User deleted'), // 204 = No Content (Sucesso sem conteúdo)
+      }
+    }
+  }, async (request, reply) => {
+    const { id } = request.params;
+
+    await prisma.user.delete({
+      where: { id }
+    });
+
+    return reply.status(204).send();
+  });
+
+  // Rota de ATUALIZAR (PUT)
+  app.put('/users/:id', {
+    schema: {
+      description: 'Atualizar dados de um usuário',
+      tags: ['users'],
+      params: z.object({
+        id: z.string().uuid(),
+      }),
+      body: z.object({
+        name: z.string().optional(),
+        email: z.string().email().optional(),
+      }),
+      response: {
+        204: z.null().describe('User updated'),
+      }
+    }
+  }, async (request, reply) => {
+    const { id } = request.params;
+    const { name, email } = request.body;
+
+    await prisma.user.update({
+      where: { id },
+      data: { name, email }
+    });
+
+    return reply.status(204).send({});
+  });
+
 }
